d29536f378e679de38cbc40aa43d415e60bedd97
Jason Stuemke <jasnstu@gmail.com>
2022-11-10 19:36:47 -0800

    Added Project to Repo

---
+++ Tests/2 - LoopsStacksAndLinkedLists/LoopsStacksAndLinkedLists/LoopsStacksAndLinkedListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopsStacksAndLinkedListsApp.swift
+//  LoopsStacksAndLinkedLists
+//
+//  Created by Jason Stuemke on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LoopsStacksAndLinkedListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
